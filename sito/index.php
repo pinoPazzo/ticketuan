45e454c5a0edbac68f51aa3127f3bd7919f47d91
leor0 <lucascudeller05@gmail.com>
2024-05-10 12:40:22 +0200

    sistema dim

--- sito/index.php
+++ sito/index.php
@@ -1,65 +1,70 @@
 <?php require_once "navBar.php" ?>
-<main class="container  flex-grow-1">
+<main class="container-fluid flex-grow-1">
     <div class="row mt-3 ">
         <h2 class="text-center">Sport</h2>
-        <div class="owl-carousel owl-theme">
-            <div class="card d-flex flex-column justify-content-center ">
-                <div class="card-body">
-                    <img class="card-img-top h-25"
-                         src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                         alt="puzzo" >
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="owl-carousel owl-theme">
+                <div class="card d-flex flex-column justify-content-center ">
+                    <div class="card-body">
+                        <img class="card-img-top h-25"
+                             src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="puzzo">
+                    </div>
+                    <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
                 </div>
-                <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
-            </div>
-            <div class="card d-flex flex-column justify-content-center ">
-                <div class="card-body">
-                    <img class="card-img-top h-25"
-                         src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                         alt="puzzo" >
+                <div class="card d-flex flex-column justify-content-center ">
+                    <div class="card-body">
+                        <img class="card-img-top h-25"
+                             src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="puzzo">
+                    </div>
+                    <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
                 </div>
-                <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
-            </div>
-            <div class="card d-flex flex-column justify-content-center ">
-                <div class="card-body">
-                    <img class="card-img-top h-25"
-                         src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                         alt="puzzo" >
+                <div class="card d-flex flex-column justify-content-center ">
+                    <div class="card-body">
+                        <img class="card-img-top h-25"
+                             src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="puzzo">
+                    </div>
+                    <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
                 </div>
-                <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
-            </div>
-            <div class="card d-flex flex-column justify-content-center ">
-                <div class="card-body">
-                    <img class="card-img-top h-25"
-                         src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                         alt="puzzo" >
+                <div class="card d-flex flex-column justify-content-center ">
+                    <div class="card-body">
+                        <img class="card-img-top h-25"
+                             src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="puzzo">
+                    </div>
+                    <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
                 </div>
-                <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
-            </div>
-            <div class="card d-flex flex-column justify-content-center ">
-                <div class="card-body">
-                    <img class="card-img-top h-25"
-                         src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                         alt="puzzo" >
+                <div class="card d-flex flex-column justify-content-center ">
+                    <div class="card-body">
+                        <img class="card-img-top h-25"
+                             src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="puzzo">
+                    </div>
+                    <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
                 </div>
-                <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
-            </div>
-            <div class="card d-flex flex-column justify-content-center ">
-                <div class="card-body">
-                    <img class="card-img-top h-25"
-                         src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                         alt="puzzo" >
+                <div class="card d-flex flex-column justify-content-center ">
+                    <div class="card-body">
+                        <img class="card-img-top h-25"
+                             src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="puzzo">
+                    </div>
+                    <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
                 </div>
-                <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
-            </div>
-            <div class="card d-flex flex-column justify-content-center ">
-                <div class="card-body">
-                    <img class="card-img-top h-25"
-                         src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                         alt="puzzo" >
+                <div class="card d-flex flex-column justify-content-center ">
+                    <div class="card-body">
+                        <img class="card-img-top h-25"
+                             src="https://images.pexels.com/photos/1435904/pexels-photo-1435904.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="puzzo">
+                    </div>
+                    <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
                 </div>
-                <button class="btn btn-outline-warning h-25 m-auto mb-2">Acquista</button>
             </div>
         </div>
+        >
     </div>
 
 </main>
