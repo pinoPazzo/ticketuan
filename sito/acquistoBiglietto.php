5754345c888c18e77bd0a7b15c0fd12add29d168
hazon24 <tommasocaccia532@gmail.com>
2024-05-14 21:47:20 +0200

    acquisto se ha voglia

--- sito/acquistoBiglietto.php
+++ sito/acquistoBiglietto.php
@@ -8,7 +8,36 @@ if($isLogged){
     if ($result->rowCount() > 0){
         $result = $result->fetchAll();
         print_r($result);?>
-        <a class="btn btn-outline-warning  m-auto mb-2" href="GestioneAcquisto/confermaBiglietto.php?id=<?=$_GET["id"]?>&prezzo=<?=$result[0]["Costo"]?>">Conferma Acquisto</a>
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col col-xl-10">
+                    <div class="card shadow" style="border-radius: 1rem;">
+                        <div class="row g-0">
+                            <div class="col-md-6 col-lg-5 d-none d-md-block">
+                                <img src="#"
+                                     alt="foto magica dell'evento" class="img-fluid" style="border-radius: 1rem 0 0 1rem;"/>
+                            </div>
+                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div class="card-body p-4 p-lg-5">
+
+                                    <form method="post" action="validazioneLogin.php">
+
+                                        <div class="d-flex align-items-center mb-3 pb-1">
+                                            <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                                            <span class="h1 fw-bold mb-0">Bentornato</span>
+                                        </div>
+
+                                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Accedi al tuo account</h5>
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     <?php }
     else{
         header("Location: index.php");
@@ -25,3 +54,5 @@ if($isLogged){
 </div>
 <?php }?>
 $_SESSION['username']
+
+<a class="btn btn-outline-warning  m-auto mb-2" href="GestioneAcquisto/confermaBiglietto.php?id=<?=$_GET["id"]?>&prezzo=<?=$result[0]["Costo"]?>">Conferma Acquisto</a>
