4650f73474cb341667e0ff7b936c412fe1e7479a
Gr4tos <danielebattaglia40@gmail.com>
2024-05-15 08:42:44 +0200

    messi l'url BATTLE USA GIT CLONE

--- database/DDL.sql
+++ database/DDL.sql
@@ -55,15 +55,18 @@ CREATE TABLE reclami(
                         PRIMARY KEY(IdReclamo)
 );
 
-INSERT INTO eventi(Artista, NomeEvento, Localita, Citta, DataOra, Costo, Categoria) VALUES ('Marracash', 'Marra Stadi 25', 'Stadio San Siro', 'Milano', '2025-06-25', 59, 'Concerto'),
-                                                                                           ('Annalisa', 'Tutti nel Vortice Outdoor', 'Arena del Mare', 'Genova', '2024-07-17 21:30:00', 40, 'Concerto'),
-                                                                                           ('Marracash', 'Marra Stadi 25', 'Stadio Diego Armando Maradona', 'Napoli', '2025-06-10 21:00:00', 69, 'Concerto'),                                                                               ('Pinguini Tattici Nucleari', 'Hello Word', 'Ippodromo di Visarno', 'Firenze', '2025-06-25 21:00:00', 49, 'Concerto'),
-                                                                                           ('Ultimo', 'Stadi 2024', 'Stadio Olimpico', 'Roma', '2024-06-22 21:00:00', 69, 'Concerto'),
-                                                                                           ('Roberto Bolle', 'Roberto Bolle and Friends', 'Teatro degli Arcimboldi', 'Milano', '2024-05-24 21:00:00', 33.50, 'Teatro'),
-                                                                                           ('Andrea Pucci', 'C\'è sempre qualcosa che non va', 'Palazzo del Turismo', 'Jesolo', '2024-06-15 21:15:00', 54, 'Teatro'),
-                                                                                           ('Awed, Dadda, Dose', 'Esperienze D.M.', 'Teatro Ariston', 'Sanremo', '2024-10-04 21:00:00', 26, 'Teatro'),
-                                                                                           (NULL, 'Gran Premio d\'Italia 2024', 'Autodromo Internazionale di Mugello', 'Firenze', '2024-06-01 00:01:00', 75, 'Sport'),
-                                                                                           (NULL, 'Davis Cup', 'Unipol Arena', 'Casalecchio di Reno', '2024-09-10 15:00:00', 44, 'Sport');
+
+INSERT INTO eventi(Artista, NomeEvento, Localita, Citta, DataOra, Costo, Categoria, URL) VALUES ('Marracash', 'Marra Stadi 25', 'Stadio San Siro', 'Milano', '2025-06-25', 59, 'Concerto', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/marrastadi25-biglietti.jpg'),
+                                                                                                ('Annalisa', 'Tutti nel Vortice Outdoor', 'Arena del Mare', 'Genova', '2024-07-17 21:30:00', 40, 'Concerto', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/annalisa-estate-biglietti.jpg'),
+                                                                                                ('Marracash', 'Marra Stadi 25', 'Stadio Diego Armando Maradona', 'Napoli', '2025-06-10 21:00:00', 69, 'Concerto', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/marrastadi25-biglietti.jpg'),
+                                                                                                ('Pinguini Tattici Nucleari', 'Hello Word', 'Ippodromo di Visarno', 'Firenze', '2025-06-25 21:00:00', 49, 'Concerto', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/ptn-stadi-biglietti.jpg'),
+                                                                                                ('Ultimo', 'Stadi 2024', 'Stadio Olimpico', 'Roma', '2024-06-22 21:00:00', 69, 'Concerto', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2023/ultimo-stadi24-biglietti.jpg'),
+                                                                                                ('Roberto Bolle', 'Roberto Bolle and Friends', 'Teatro degli Arcimboldi', 'Milano', '2024-05-24 21:00:00', 33.50, 'Teatro', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/roberto-bolle-friends-biglietti.jpg'),
+                                                                                                ('Andrea Pucci', 'C\'è sempre qualcosa che non va', 'Palazzo del Turismo', 'Jesolo', '2024-06-15 21:15:00', 54, 'Teatro', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2022/pucci-non-va-biglietti.jpg'),
+                                                                                                ('Awed, Dadda, Dose', 'Esperienze D.M.', 'Teatro Ariston', 'Sanremo', '2024-10-04 21:00:00', 26, 'Teatro', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/esperienze-dm-biglietti-2.jpg'),
+                                                                                                (NULL, 'Gran Premio d\'Italia 2024', 'Autodromo Internazionale di Mugello', 'Firenze', '2024-06-01 00:01:00', 75, 'Sport', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/motogp-mugello-biglietti.jpg'),
+                                                                                                (NULL, 'Davis Cup', 'Unipol Arena', 'Casalecchio di Reno', '2024-09-10 15:00:00', 44, 'Sport', 'https://www.ticketone.it/obj/media/IT-eventim/teaser/222x222/2024/davis-cup-biglietti.png');
+
 
 SELECT * FROM clienti;
 
